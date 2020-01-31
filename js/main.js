74dcca9f2895c1fb052469129b2dc97ad9e9db3b
Tenzil <l.tenzil@gmail.com>
2020-01-31 17:48:46 +0530

    Added offline page and catched sw registration error

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
+	  navigator.serviceWorker.register('./sw.js')
+	  .then(function(registration) {
+	    console.log('Registered:', registration);
+	  })
+	  .catch(function(error) {
+	    console.log('Registration failed: ', error);
+	  });
+	}
 }
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
   './',
-  './index.html',
+  './offline.html',
   './css/style.css',
   './js/main.js'
 ];
